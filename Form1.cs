2a35282fd6d5cbc4797308a237684efdc842475e
AdithyaShenal <washenal55@gmail.com>
2025-07-05 09:33:29 +0530

    model folders created

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace employee_management_system
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }

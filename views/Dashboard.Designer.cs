b37cf8b93dcb0498aed90e1d665e6f6e4298f91e
Adithya Shenal <washenal55@gmail.com>
2025-07-14 19:46:40 +0530

    dashboard added and Dashboard confilcts resolved

--- views/Dashboard.Designer.cs
+++ views/Dashboard.Designer.cs
@@ -2,36 +2,159 @@
 {
     partial class Dashboard
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+
+        // Controls from Form1
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
 
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Teal;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(363, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1023, 151);
+            this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+
+            // 
+            // button1
+            // 
+
+            // this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.Teal;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(1500, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 89);
+            this.button1.TabIndex = 10;
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoEllipsis = true;
+            this.label2.BackColor = System.Drawing.Color.Teal;
+            this.label2.Enabled = false;
+            this.label2.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(3, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 52);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Dashboard";
+            //this.label2.Click += new System.EventHandler(this.label2_Click);
+
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoEllipsis = true;
+            this.label1.BackColor = System.Drawing.Color.Teal;
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Verdana", 28.2F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 28);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.label1.Size = new System.Drawing.Size(381, 184);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "University Of Ruhuna";
+            //this.label1.Click += new System.EventHandler(this.label1_Click);
+
+            // 
+            // button6
+            // 
+            //this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.Teal;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold);
+            this.button6.ForeColor = System.Drawing.Color.Teal;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.Location = new System.Drawing.Point(50, 900);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(95, 89);
+            this.button6.TabIndex = 9;
+            this.button6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button6.UseVisualStyleBackColor = false;
+
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(363, 607);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1386, 607);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Dashboard";
             this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Dashboard_Load);
+
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
         }
 
         #endregion
--- views/Dashboard.cs
+++ views/Dashboard.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace employee_management_system.views
 {
@@ -15,6 +16,111 @@ namespace employee_management_system.views
         public Dashboard()
         {
             InitializeComponent();
+            this.Load += Dashboard_Load;
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            // Sidebar button generator
+            Button CreateSidebarButton(string text, Point location)
+            {
+                RoundedButton btn = new RoundedButton(); // Make sure RoundedButton exists in this project
+                btn.Text = text;
+                btn.Size = new Size(190, 65);
+                btn.Location = location;
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.ForeColor = Color.White;
+                btn.BackColor = Color.Teal;
+                btn.FlatAppearance.BorderSize = 3;
+                btn.FlatAppearance.BorderColor = Color.White;
+                btn.Font = new Font("Microsoft Sans Serif", 15, FontStyle.Bold);
+                btn.Cursor = Cursors.Hand;
+                return btn;
+            }
+
+            int startY = 250;
+            int spacing = 90;
+            panel1.Controls.Add(CreateSidebarButton("Employees", new Point(20, startY + spacing * 0)));
+            panel1.Controls.Add(CreateSidebarButton("Payroll", new Point(20, startY + spacing * 1)));
+            panel1.Controls.Add(CreateSidebarButton("Attendance", new Point(20, startY + spacing * 2)));
+            panel1.Controls.Add(CreateSidebarButton("Events", new Point(20, startY + spacing * 3)));
+            panel1.Controls.Add(CreateSidebarButton("Leave Management", new Point(20, startY + spacing * 4)));
+
+            this.Size = Screen.PrimaryScreen.WorkingArea.Size;
+            this.Location = Screen.PrimaryScreen.WorkingArea.Location;
+
+            // Bar chart
+            Chart chart = new Chart();
+            chart.Size = new Size(600, 500);
+            chart.Location = new Point(400, 200);
+            chart.ChartAreas.Add(new ChartArea());
+            chart.BackColor = Color.White;
+            chart.Titles.Add("Monthly Users");
+
+            Series series = new Series
+            {
+                Color = Color.CornflowerBlue,
+                ChartType = SeriesChartType.Column
+            };
+            series.Points.AddXY("Jan", 10);
+            series.Points.AddXY("Feb", 20);
+            series.Points.AddXY("Mar", 15);
+            series.Points.AddXY("Apr", 10);
+            series.Points.AddXY("May", 20);
+            series.Points.AddXY("Jun", 15);
+            series.Points.AddXY("Jul", 10);
+            series.Points.AddXY("Aug", 20);
+            series.Points.AddXY("Sep", 15);
+            series.Points.AddXY("Oct", 12);
+            series.Points.AddXY("Nov", 20);
+            series.Points.AddXY("Dec", 15);
+
+            chart.Series.Add(series);
+            this.Controls.Add(chart);
+
+            // Pie chart
+            Chart pieChart = new Chart();
+            pieChart.Size = new Size(600, 500);
+            pieChart.Location = new Point(950, 200);
+            pieChart.ChartAreas.Add(new ChartArea());
+
+            Series pieSeries = new Series
+            {
+                ChartType = SeriesChartType.Pie
+            };
+            pieSeries.Points.AddXY("HR", 30);
+            pieSeries.Points.AddXY("Payroll", 20);
+            pieSeries.Points.AddXY("Attendance", 50);
+
+            pieChart.Series.Add(pieSeries);
+            pieChart.Titles.Add("Department Ratio");
+            this.Controls.Add(pieChart);
+        }
+
+        // Optional hover effect if you're using button1
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.MediumSeaGreen;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.DarkSeaGreen;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // Reserved
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            // Reserved
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Reserved
         }
     }
 }

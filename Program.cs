f959c2084dded9e8b51f913b5d885f148df3492f
Shalin-Nimesha <shalinnimesha8@gmail.com>
2025-07-28 23:13:48 +0530

    shalin program.cs

--- Program.cs
+++ Program.cs
@@ -11,12 +11,13 @@ namespace employee_management_system
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new views.EmployeeManagementNew());
+            Application.Run();
         }
     }
 }
